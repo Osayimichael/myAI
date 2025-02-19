2b85758f2f0269a32ce1c8d8c00d035db5c38155
Osayi Eromhonsele <osayimichael@gmail.com>
2025-02-19 14:13:46 -0500

    Update identity.ts
    Changed AI name and Owners Name

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
+export const OWNER_NAME: string = `OSAYI`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `PRODUCT GENIUS`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
