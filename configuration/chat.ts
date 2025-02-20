7327fa0ac1cc4f8ba5ad6557fdf9d636b0a7e2cc
Osayi Eromhonsele <osayimichael@gmail.com>
2025-02-20 14:29:07 -0500

    Update chat.ts
    Edited initial message

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, an AI assistant with expertise in Tech Product Management interviews. How can I help?`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, an AI assistant with expertise in tech product management interviews. How can I help?`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `I am a little stressed, let's take a break please`;
