1762c26e7a15ccc39269b14ff649cb68338cee2b
Osayi Eromhonsele <osayimichael@gmail.com>
2025-02-20 13:48:35 -0500

    Update ui.ts
    Edited footer and page title

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,9 +2,9 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Dobby by Osayi, the Tech PM Wizard`;
 export const MESSAGE_PLACEHOLDER: string = `What do you want to know?`;
-export const FOOTER_MESSAGE: string = `Customized Chatbot by Osayi Eromhonsele`;
+export const FOOTER_MESSAGE: string = `© Osayi Eromhonsele 2025`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
-export const PAGE_TITLE: string = `Ace your PM Interviews`;
+export const PAGE_TITLE: string = `Ace your Tech PM Interviews`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
