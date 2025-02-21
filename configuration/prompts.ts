4a58d0bac595d93f1580d02183321ecb07446aff
Osayi Eromhonsele <osayimichael@gmail.com>
2025-02-20 21:10:56 -0500

    Update prompts.ts
    Added url links

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -43,7 +43,7 @@ export function RESPOND_TO_QUESTION_SYSTEM_PROMPT(context: string) {
   return `
 ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE} ${AI_TONE}
 
-Use the following excerpts from ${OWNER_NAME} to answer the user's question. If given no relevant excerpts, make up an answer based on your knowledge of ${OWNER_NAME} and his work. Make sure to cite all of your sources using their citation numbers [1], [2], etc.
+Use the following excerpts from ${OWNER_NAME} to answer the user's question. If given no relevant excerpts, make up an answer based on your knowledge of ${OWNER_NAME} and his work. Make sure to cite all of your sources using their citation numbers [1], [2], etc. Make sure to provide url links for each source.
 
 Excerpts from ${OWNER_NAME}:
 ${context}
