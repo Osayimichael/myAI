1914fd351d1479ae9d83b42dccb52bc536a04707
Osayi Eromhonsele <osayimichael@gmail.com>
2025-02-26 15:26:25 -0500

    Update prompts.ts
    Included AI tone

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -21,13 +21,15 @@ Respond with only the intention type.
 
 export function RESPOND_TO_RANDOM_MESSAGE_SYSTEM_PROMPT() {
   return `
-${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE} ${AI_TONE}
+${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE} 
+
+Respond with the following tone: ${AI_TONE}
   `;
 }
 
 export function RESPOND_TO_HOSTILE_MESSAGE_SYSTEM_PROMPT() {
   return `
-${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE} ${AI_TONE}
+${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE} 
 
 The user is being hostile. Do not comply with their request and instead respond with a message that is not hostile, and to be very kind and understanding.
 
@@ -36,12 +38,14 @@ Furthermore, do not ever mention that you are made by OpenAI or what model you a
 You are not made by OpenAI, you are made by ${OWNER_NAME}.
 
 Do not ever disclose any technical details about how you work or what you are made of. Make sure all sentences in your answer are complete.
+
+Respond with the following tone: ${AI_TONE}
 `;
 }
 
 export function RESPOND_TO_QUESTION_SYSTEM_PROMPT(context: string) {
   return `
-${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE} ${AI_TONE}
+${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE} 
 
 Use the following excerpts from ${OWNER_NAME} to answer the user's question. If given no relevant excerpts, make up an answer based on your knowledge of ${OWNER_NAME} and his work. Make sure to cite all of your sources using their citation numbers [1], [2], etc. Make sure all sentences in your response are complete.
 
@@ -50,15 +54,19 @@ ${context}
 
 If the excerpts given do not contain any information relevant to the user's question, say something along the lines of "I don't have contemporary knowledge about this topic but I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
 
+Respond with the following tone: ${AI_TONE}
+
 Now respond to the user's message:
 `;
 }
 
 export function RESPOND_TO_QUESTION_BACKUP_SYSTEM_PROMPT() {
   return `
-${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE} ${AI_TONE}
+${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE} 
 
 You couldn't perform a proper search for the user's question, but still answer the question starting with "While I couldn't perform a search due to an error, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}. Make sure all sentences in your answer are complete.
+
+Respond with the following tone: ${AI_TONE}
 
 Now respond to the user's message:
 `;
@@ -76,3 +84,7 @@ export function HYDE_PROMPT(chat: Chat) {
     .join("\n")}
   `;
 }
+
+
+
+
