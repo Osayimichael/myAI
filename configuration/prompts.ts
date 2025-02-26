0a335667468c059d7bdf0a08f4e153ef2ce8992e
Osayi Eromhonsele <osayimichael@gmail.com>
2025-02-26 16:44:59 -0500

    Update prompts.ts
    Remove owner name from responses

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -47,7 +47,7 @@ export function RESPOND_TO_QUESTION_SYSTEM_PROMPT(context: string) {
   return `
 ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE} 
 
-Use the following excerpts from ${OWNER_NAME} to answer the user's question. If given no relevant excerpts, make up an answer based on your knowledge of ${OWNER_NAME} and his work. Make sure to cite all of your sources using their citation numbers [1], [2], etc. Make sure all sentences in your responses are complete.
+Use the following excerpts from ${OWNER_NAME} to answer the user's question. If given no relevant excerpts, make up an answer based on your knowledge of ${OWNER_NAME} and his work. Make sure to cite all of your sources using their citation numbers [1], [2], etc. Make sure all sentences in your responses are complete. Do not mention ${OWNER_NAME} in your responses.
 
 Excerpts from ${OWNER_NAME}:
 ${context}
@@ -64,7 +64,7 @@ export function RESPOND_TO_QUESTION_BACKUP_SYSTEM_PROMPT() {
   return `
 ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE} 
 
-You couldn't perform a proper search for the user's question, but still answer the question starting with "While I couldn't perform a search due to an error, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}. Make sure all sentences in your responses are complete.
+You couldn't perform a proper search for the user's question, but still answer the question starting with "While I couldn't perform a search due to an error, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}. Make sure all sentences in your responses are complete. Do not mention ${OWNER_NAME} in your responses.
 
 Respond with the following tone: ${AI_TONE}
 
