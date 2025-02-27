f026556d9674c0218ee7a882926b886cace075f9
Osayi Eromhonsele <osayimichael@gmail.com>
2025-02-26 22:20:17 -0500

    Update ui.ts
    Included warning as footer

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,7 +2,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Dobby by Osayi, the Tech PM Wizard`;
 export const MESSAGE_PLACEHOLDER: string = `What do you want to know?`;
-export const FOOTER_MESSAGE: string = `© Osayi Eromhonsele 2025`;
+export const FOOTER_MESSAGE: string = `Dobby can make mistakes. Check Important info.`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
 export const PAGE_TITLE: string = `Ace your Tech PM Interviews`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
