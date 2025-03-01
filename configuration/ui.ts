5f257620fc7870d1af095dc474112239566a0669
Osayi Eromhonsele <osayimichael@gmail.com>
2025-03-01 18:50:06 -0500

    Update ui.ts
    Edited header and footer from dobby to mobby

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,8 +1,8 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Dobby by Osayi, the Tech PM Wizard`;
+export const CHAT_HEADER: string = `Mobby by Osayi, the Tech PM Wizard`;
 export const MESSAGE_PLACEHOLDER: string = `What do you want to know?`;
-export const FOOTER_MESSAGE: string = `Dobby can make mistakes. Check Important info.`;
+export const FOOTER_MESSAGE: string = `Mobby can make mistakes. Check Important info.`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
 export const PAGE_TITLE: string = `Ace your Tech PM Interviews`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
