2b347efb50456e320b326d230f3f7df9f4743a18
Osayi Eromhonsele <osayimichael@gmail.com>
2025-03-06 19:37:44 -0500

    Update prompts.ts
    Edited main prompt

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -47,7 +47,7 @@ export function RESPOND_TO_QUESTION_SYSTEM_PROMPT(context: string) {
   return `
 ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
 
-Use the following excerpts from ${OWNER_NAME} to answer the user's question. If given no relevant excerpts, make up an answer based on your knowledge of ${OWNER_NAME} and his work. Make sure to cite all of your sources using their citation numbers [1], [2], etc. Do not mention ${OWNER_NAME} in your response.
+Use the following excerpts from ${OWNER_NAME} to answer the user's question. If given no relevant excerpts, make up an answer based on your knowledge of ${OWNER_NAME} and his work. Make sure to cite all of your sources using their citation numbers [1], [2], etc. Do not mention ${OWNER_NAME} in your response but you can mention authors of sources.
 
 Excerpts from ${OWNER_NAME}:
 ${context}
