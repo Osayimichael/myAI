a4eb6c257bd1f887f42bce9723f6571bd2bb77b8
Osayi Eromhonsele <osayimichael@gmail.com>
2025-03-08 20:00:20 -0500

    Update footer.tsx
    Edited footer to match header

--- components/chat/footer.tsx
+++ components/chat/footer.tsx
@@ -3,28 +3,28 @@ import Link from "next/link";
 
 export default function ChatFooter() {
   return (
-    <div className="w-full text-xs flex pt-2 text-gray-500">
-      <div className="flex-grow text-left">
+    <footer className="w-full bg-white shadow-md text-xs text-gray-600 py-3 flex items-center">
+      <div className="flex-1 text-left pl-4">
         {/* Left Pane */}
-        <Link href="/terms" className="hover:underline">
+        <Link href="/terms" className="hover:underline hover:text-gray-800">
           Terms of Use
         </Link>
       </div>
-      <div className="flex-grow text-center">
+      <div className="flex-1 text-center">
         {/* Center Pane */}
         {FOOTER_MESSAGE}
       </div>
-      <div className="flex-grow text-right">
+      <div className="flex-1 text-right pr-4">
         {/* Right Pane */}
-        {/* Do not remove or modify the following message. Removal or modification violates the license agreement. */}
         <a
           href="http://www.ringel.ai"
           target="_blank"
           rel="noopener noreferrer"
+          className="hover:text-gray-800"
         >
           Powered by ringel.AI
         </a>
       </div>
-    </div>
+    </footer>
   );
 }
