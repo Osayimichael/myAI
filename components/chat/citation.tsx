59b8bd0b58a50fb54d25bb76cb70043a5cf0da66
Osayi Eromhonsele <osayimichael@gmail.com>
2025-03-16 17:48:40 -0400

    pushed original commit
    Pushed initial commit from dr ringel

--- components/chat/citation.tsx
+++ components/chat/citation.tsx
@@ -2,7 +2,11 @@
 
 import { useState } from "react";
 import { Citation } from "@/types";
-import { Tooltip } from "react-tippy";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
 import Link from "next/link";
 import { EMPTY_CITATION_MESSAGE } from "@/configuration/ui";
 
@@ -27,6 +31,7 @@ export function CitationCircle({
   const hasSourceDescription = citation.source_description.trim() !== "";
 
   return (
+<<<<<<< HEAD
     <Tooltip
       title=""
       open={open}
@@ -36,6 +41,18 @@ export function CitationCircle({
       trigger="mouseenter click"
       interactive={true}
       html={
+=======
+    <Tooltip open={open} onOpenChange={setOpen}>
+      <TooltipTrigger>
+        <div
+          className="bg-gray-50 rounded-full px-2 py-0.5 hover:cursor-pointer hover:scale-105 inline-block"
+          onClick={() => setOpen(true)}
+        >
+          <span>{number}</span>
+        </div>
+      </TooltipTrigger>
+      <TooltipContent>
+>>>>>>> parent of 3a725fa (Reapply "Merge branch 'dringel:main' into main")
         <div className="bg-white p-2 rounded-md shadow-sm flex flex-col justify-center border-[1px] border-gray-200">
           <p>
             {hasSourceUrl && (
@@ -51,7 +68,12 @@ export function CitationCircle({
             {!hasSourceUrl && !hasSourceDescription && EMPTY_CITATION_MESSAGE}
           </p>
         </div>
+<<<<<<< HEAD
       }
     ></Tooltip>
+=======
+      </TooltipContent>
+    </Tooltip>
+>>>>>>> parent of 3a725fa (Reapply "Merge branch 'dringel:main' into main")
   );
 }
